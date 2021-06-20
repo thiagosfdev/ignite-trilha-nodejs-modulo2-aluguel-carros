c929c3202ac9e945db925fd5fe0b83fcae8dbd9b
C1300284 Thiago Soares Figueiredo <c1300284@interno.bb.com.br>
2021-06-20 14:49:24 -0300

    Configiração do tsconfig

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,11 @@
-import express from 'express';
+import express from "express";
 
 const APP = express();
 const PORT = 3333;
+
+APP.get("/", (_req, res) => {
+    return res.json({ api: "OK" });
+});
 
 APP.listen(PORT, () => {
     console.log(`Server running in http://localhost:${PORT}`);
