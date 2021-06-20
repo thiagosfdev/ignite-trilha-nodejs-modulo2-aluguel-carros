6072095920ef41f2bfef9e2a78a4432153e2bfc1
C1300284 Thiago Soares Figueiredo <c1300284@interno.bb.com.br>
2021-06-20 19:00:04 -0300

    Criando classe model

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,18 @@
 import express from "express";
 
+import { categoriesRoutes } from "./routes/categories";
+
 const APP = express();
 const PORT = 3333;
+
+APP.use(express.urlencoded({ extended: true }));
+APP.use(express.json());
 
 APP.get("/", (_req, res) => {
     return res.json({ api: "OK" });
 });
+
+APP.use("/categories", categoriesRoutes);
 
 APP.listen(PORT, () => {
     console.log(`Server running in http://localhost:${PORT}`);
